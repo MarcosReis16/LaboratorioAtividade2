d23567abc8f2790c523f844d4aabcb38ac6b01f5
Marcos Reis <marcossummers@gmail.com>
2018-08-09 20:18:16 -0300

    Primeiros passos

---
+++ ModuloFuncoes.c
@@ -0,0 +1,12 @@
+int menu(){
+    int op;
+    printf("Digite as opção desejada\n");
+    printf("0 - Sair\n");
+    printf("1 - Inserir\n");
+    printf("2 - Excluir\n");
+    printf("3 - Listar\n");
+    printf("4 - \n");
+    printf("5 - \n");
+    scanf("%d", &op);
+    return op;
+}
--- main.c
+++ main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "ModuloFuncoes.h"
 
-int main()
-{
-    printf("Hello world!\n");
+int main(){
+
+    int op,i,j;
+    int sair = 0;
+    VetorPrincipal vet[10];
+
+
+    while (!sair){
+        op = menu();
+        switch (op){
+            case 0:{
+                sair =1;
+                break;
+            }
+            case 1:{ //inserir
+                printf("Digite a posicao do vetor que deseja inserir um valor:\n");
+                scanf("%d", &i);
+
+                if (vet[i].auxiliar == NULL){
+                    printf("Nosso vetor ainda nao tem um tamanho. Digite um tamanho para ele:\n");
+                    scanf("%d", &j);
+                    vet[i].auxiliar = (int*)malloc(sizeof(int)*j);
+                }
+
+
+
+                break;
+            }
+
+            case 2:{ //excluir
+                //TODO
+                break;
+            }
+            default:{
+                printf("opcao inválida\n");
+            }
+
+
+        }
+
+
+    }
+
     return 0;
+
 }
