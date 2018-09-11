99b9885d2d98446c9b2c428447a47cb869396f2e
Marcos Reis <marcossummers@gmail.com>
2018-09-11 18:36:12 -0300

    Inserindo,Listando e Realocando

--- main.c
+++ main.c
@@ -5,19 +5,20 @@
 
 int main(){
 
-    int op,i,valor;
+    int op,i,valor,num;
     int sair = 0;
     VetorPrincipal vet[10];
     for(i=0;i<10;i++){
         vet[i].auxiliar = NULL;
         vet[i].cont = 0;
+        vet[i].tamanho = 0;
     }
 
 
     while (!sair){
         op = menu();
         switch (op){
-            case 0:{
+            case 0:
                 for(i=0;i<=9;i++){
                     if(vet[i].auxiliar != NULL){
                         free(vet[i].auxiliar);
@@ -25,25 +26,25 @@ int main(){
                 }
                 sair =1;
                 break;
-            }
-            case 1:{ //inserir
+
+            case 1: //inserir
                 printf("Digite a posicao (1-10) do vetor que deseja inserir um valor:\n");
                 scanf("%d", &i);
 
                 if(i>=1 && i<=10){
-
-                        if (vet[i-1].auxiliar == NULL){
+                        i = i-1;
+                        if (vet[i].auxiliar == NULL){
                             printf("Nosso vetor ainda nao tem um tamanho. Digite um tamanho para ele:\n");
-                            scanf("%d", &vet[i-1].tamanho);
-                            alocarMemoria(vet,i-1);
+                            scanf("%d", &vet[i].tamanho);
+                            alocarMemoria(vet,i);
 
                             printf("Digite um valor para inserir:\n");
                             scanf("%d",&valor);
-                            inserirValor(vet,i-1,valor);
+                            inserirValor(vet,i,valor);
 
                         }
                         else{
-                            if(vet[i-1].cont == vet[i-1].tamanho){
+                            if(vet[i].cont == vet[i].tamanho){
                                 printf("Nosso vetor ja atingiu seu limite, precisamos alocar mais espaço.\n");
                             }
                             else{
@@ -57,47 +58,51 @@ int main(){
 
 
                 break;
-            }
 
-            case 2:{ //excluir
+
+            case 2: //excluir
                 //TODO
                 printf("Digite a posicao do vetor principal que deseja apagar um valor:\n");
                 scanf("%d", &i);
-
+                i=i-1;
                 if(vet[i].auxiliar == NULL){
                     printf("Não existe um vetor alocado neste índice.");
                 }
                 else{
-                    printf("Escolha um valor para apagar do vetor");
+                    printf("Escolha um valor para apagar do vetor:\n");
                     scanf("%d",&valor);
                     apagaValorMemoria(vet,i,valor);
                 }
                 break;
-            case 3:{//listar
+
+            case 3://listar
                 printf("Digite a posicao do vetor principal que deseja listar: \n");
                 scanf("%d",&i);
-
-                if(vet[i-1].auxiliar!=NULL)
-                    listaVetor(vet,i-1);
+                i=i-1;
+                if(vet[i].auxiliar!=NULL)
+                    listaVetor(vet,i);
                 else
                     printf("Este vetor esta vazio.\n");
 
                 break;
-            }
-            case 4:{
-                printf("Digite a opcao que deseja realocar espaço: \n");
-                scanf("%d",&i);
 
+            case 4:
+                printf("Digite a posicao que deseja realocar espaço: \n");
+                scanf("%d",&i);
+                i=i-1;
                 if(vet[i].auxiliar != NULL){
                     printf("Digite o novo tamanho do vetor: \n");
                     scanf("%d",&vet[i].tamanho);
                     realocarMemoria(vet,i);
                 }
-            }
-            }
-            default:{
+                else
+                    printf("Nao ha espaco alocado para esse vetor.\n");
+
+                break;
+
+            default:
                 printf("opcao inválida\n");
-            }
+
         }
     }
     return 0;
